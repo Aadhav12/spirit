fce3f53eeb3325a1f720ab51c9941e63f788447c
Aadhav12 <aadhav.nicodiangelo@gmail.com>
2026-02-06 18:03:09 +0530

    World tilemap and house generation
    - The tilemap has been created for the world with grass, sand, and oceans
    - The tilemap has been implemented in the world scene.
    - The house generation has been started at a very basic level. See below for details.
    
    House generation details.
    1. Takes size and number of beds as input.
    2. Creates one main rectangle and one offshoot rectangle to form a house shape.
    3. Places beds, prioritising corners and having the beds on the same y level for better looking houses.

---
+++ scripts/village.gd
@@ -0,0 +1,32 @@
+extends Node2D
+
+@export var population : int = 0
+@export var house_size : int
+@export var num_beds : int
+const house_scene = preload("res://scenes/house.tscn")
+var houses : Array[House] = []
+var house_instance
+
+func check_house_demand():
+	if len(houses) < population / 4 - population % 4:
+		return true
+	else:
+		return false
+
+func _ready():
+	house_instance = house_scene.instantiate()
+	var percentage_main = randi_range(70, 85)
+	house_instance.house_main_size = (float(percentage_main) / 100) * house_size
+	house_instance.house_offshoot_size = house_size - (float(percentage_main) / 100) * house_size
+	house_instance.num_beds = num_beds
+	add_child(house_instance)
+
+func _process(delta):
+	if (Input.is_key_pressed(KEY_A)):
+		remove_child(house_instance)
+		house_instance = house_scene.instantiate()
+		var percentage_main = randi_range(70, 85)
+		house_instance.house_main_size = (float(percentage_main) / 100) * house_size
+		house_instance.house_offshoot_size = house_size - (float(percentage_main) / 100) * house_size
+		house_instance.num_beds = num_beds
+		add_child(house_instance)
