6c8193a40e28b2c911ba1403bb943d0d8b293b21
aarnaahuja2010-lab <aarnaahuja2010@gmail.com>
2026-02-19 22:36:26 +0530

    enemy movement
    fixed it

--- scripts/enemy_walk_state.gd
+++ scripts/enemy_walk_state.gd
@@ -1,22 +1,13 @@
 extends NodeState
 
-@export var character: CharacterBody2D
 @export var sprite_2D: Sprite2D #change to animated
-		
-func _on_process(_delta : float) -> void:
-	pass
+@export var enemy: Enemy
+@export var speed: float = 25
 
-func _on_physics_process(_delta : float) -> void:
-	#add animation
-	pass
-	
-func _on_next_transitions() -> void:
-	pass
 
-func _on_enter() -> void:
-	pass
-	#play animation
-
-func _on_exit() -> void:
-	pass
-	#stop animation
+func _physics_process(_delta: float) -> void:
+	#if towers destroyed
+	var player_position = enemy.player.global_position
+	var target_position = (player_position - enemy.global_position).normalized()
+	enemy.velocity = target_position * speed
+	enemy.move_and_slide()
