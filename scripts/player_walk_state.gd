3242ddb70614b4b373f3daef393737d316618216
aarnaahuja2010-lab <aarnaahuja2010@gmail.com>
2026-02-20 02:07:44 +0530

    player animations
    committing to get collision on walls

--- scripts/player_walk_state.gd
+++ scripts/player_walk_state.gd
@@ -1,7 +1,7 @@
 extends NodeState
 
 @export var player: Player
-@export var sprite_2D: Sprite2D #change to animated
+@export var animated_sprite_2D: AnimatedSprite2D 
 @export var speed: int = 50
 
 func _on_process(_delta : float) -> void:
@@ -9,8 +9,6 @@ func _on_process(_delta : float) -> void:
 
 func _on_physics_process(_delta : float) -> void:
 	var direction: Vector2 = GameInputEvents.movement_input()
-	
-	"""
 	if direction == Vector2.UP:
 		animated_sprite_2D.play("walk_back")
 	elif direction == Vector2.DOWN:
@@ -19,7 +17,6 @@ func _on_physics_process(_delta : float) -> void:
 		animated_sprite_2D.play("walk_left")
 	elif direction == Vector2.RIGHT:
 		animated_sprite_2D.play("walk_right")
-	"""
 	
 	if direction != Vector2.ZERO:
 		player.player_direction = direction
@@ -37,4 +34,3 @@ func _on_enter() -> void:
 
 func _on_exit() -> void:
 	pass
-	#sprite_2D.stop() change to animated too
