e48d755f0c2a73cba76a6e2d0e258afa1243c428
Aadhav12 <aadhav.nicodiangelo@gmail.com>
2026-02-20 02:00:13 +0530

    Shop UI
    - Added shop
    - Added coin system
    - Added ability to buy altars

--- scripts/village.gd
+++ scripts/village.gd
@@ -2,6 +2,8 @@ class_name Village
 extends TileMapLayer
 
 @export var population : int = 0
+@export var upgrade_paths : TileMapLayer
+@export var world : Node2D
 
 const village_hall_scene = preload("res://scenes/village_hall.tscn")
 const house_scene = preload("res://scenes/house.tscn")
@@ -12,12 +14,16 @@ var altar_instance : Altar
 var house_generator : HouseGenerator
 var cellStatus : Dictionary
 
+
 var altars = {}
 var astar
 
 var houses : Array[House] = []
 
+
+
 func _ready():
+	world.user_interface.buy_pressed.connect(buy_altar)
 	astar = AStarGrid2D.new()
 	astar.region = Rect2i(-200, -200, 400, 400)
 	astar.cell_size = Vector2i(1, 1)
@@ -40,6 +46,14 @@ func _ready():
 	for i in range(0, 9):
 		for j in range(0, 7):
 			cellStatus[Vector2i(i, j)] = 2
+
+func buy_altar(item_data):
+	if GameData.coins >= GameData.AltarCosts[len(altars.keys())]:
+		world.user_interface.reduce_coins(GameData.AltarCosts[len(altars.keys())])
+		if item_data.type == "altar":
+			if not altars.has(GameData.spirit_names[item_data.name]):
+				create_house(GameData.spirit_names[item_data.name], 20, 2)
+				altars[GameData.spirit_names[item_data.name]].create_villager(0)
 
 func create_house(school : GameData.School, house_size : int, num_beds : int):
 	if not altars.has(school):
@@ -75,13 +89,16 @@ func create_altar(school : GameData.School):
 	var altar_data = AltarData.new()
 	altar_data.school = school
 	altar_data.position = selected
+	altar_data.door = Vector2i(2, 4)
 	for i in range(0, 5):
 		for j in range(0, 5):
 			altar_data.cellList.append(Vector2i(i, j))
 	altars[school] = altar_instance
 	altar_instance.data = altar_data
-	
+	for i in range(0, len(GameData.shop_data['altars'])):
+		GameData.shop_data['altars'][i].price = str(GameData.AltarCosts[len(altars.keys())])
 	add_child(altar_instance)
+	print(GameData.shop_data['altars'][1].price)
 	
 func create_path(a: Vector2i, b: Vector2i):
 	# Mark blocked cells
@@ -89,7 +106,7 @@ func create_path(a: Vector2i, b: Vector2i):
 		if astar.is_in_boundsv(cell) and cellStatus[cell] == 2:
 			astar.set_point_solid(cell, true)
 		if cellStatus[cell] == 1:
-			astar.set_point_weight_scale(cell, 0.25)
+			astar.set_point_weight_scale(cell, 0.1)
 	
 	var path = astar.get_id_path(a, b)
 	path.append(a + Vector2i(0, -1))
@@ -111,5 +128,3 @@ func get_village_path(a : Vector2i, b : Vector2i):
 		if cellStatus[cell] == 1:
 			astar.set_point_weight_scale(cell, 0.05)
 	return astar.get_id_path(a, b)
-	
-	
